a71324e2a2c482ed9b8d6933c3edab10aaa8ead6
Milan Shah <mshah1992@live.com>
2020-12-21 14:31:51 -0500

    Added eureka discovery service

---
+++ PhotwoAppDiscoveryService/src/main/java/com/photwo/app/discovery/PhotwoAppDiscoveryService/PhotwoAppDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.photwo.app.discovery.PhotwoAppDiscoveryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotwoAppDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotwoAppDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ PhotwoAppDiscoveryService/src/test/java/com/photwo/app/discovery/PhotwoAppDiscoveryService/PhotwoAppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photwo.app.discovery.PhotwoAppDiscoveryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotwoAppDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
