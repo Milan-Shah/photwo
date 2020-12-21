372eab99dd66e59da3acb7bb35b8d62a6e647d46
Milan Shah <mshah1992@live.com>
2020-12-21 15:04:28 -0500

    Added eureka server configuration

--- PhotwoAppDiscoveryService/src/main/java/com/photwo/app/discovery/PhotwoAppDiscoveryService/PhotwoAppDiscoveryServiceApplication.java
+++ PhotwoAppDiscoveryService/src/main/java/com/photwo/app/discovery/PhotwoAppDiscoveryService/PhotwoAppDiscoveryServiceApplication.java
@@ -2,8 +2,10 @@ package com.photwo.app.discovery.PhotwoAppDiscoveryService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class PhotwoAppDiscoveryServiceApplication {
 
 	public static void main(String[] args) {
