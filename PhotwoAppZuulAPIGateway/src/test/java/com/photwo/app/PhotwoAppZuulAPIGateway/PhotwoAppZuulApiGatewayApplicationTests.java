e2db95d1fe53412e6245821b0ec3fe646960bc10
Milan Shah <mshah1992@live.com>
2020-12-22 08:43:19 -0500

    Added ZUUL API Gateway for load balance microservices

---
+++ PhotwoAppZuulAPIGateway/src/test/java/com/photwo/app/PhotwoAppZuulAPIGateway/PhotwoAppZuulApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photwo.app.PhotwoAppZuulAPIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotwoAppZuulApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
